b9f109dd935f597fc654d11c741a4158cd8bb369
Mudar <mudarshullar22@gmail.com>
2026-06-22 18:54:13 +0200

    add if exists condition in db_notebook & add it again into the data pipeline

--- databricks/notebooks/db_notebook.py
+++ databricks/notebooks/db_notebook.py
@@ -1,19 +1,25 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC create catalog cars_catalog;
+# MAGIC create catalog if not exists cars_catalog
 
 # COMMAND ----------
 
-# creating schema for silver and gold layer
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC create schema cars_catalog.silver
+# creating schema for the three layers (bronze, silver and gold)
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC create schema cars_catalog.gold
+# MAGIC create schema if not exists cars_catalog.bronze
 
 # COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema if not exists cars_catalog.silver
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema if not exists cars_catalog.gold
+
+# COMMAND ----------
+
