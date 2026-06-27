2a0366c99cdfcae8c04c9f6f243adf3badfd95a5
Mudar <mudarshullar22@gmail.com>
2026-06-27 12:11:52 +0200

    add external location

--- databricks/notebooks/db_notebook.py
+++ databricks/notebooks/db_notebook.py
@@ -21,5 +21,10 @@
 # MAGIC %sql
 # MAGIC create schema if not exists cars_catalog.gold
 
+# MAGIC %sql
+# MAGIC create external location if not exists landing_zone
+# url 'abfss://landing-zone@lakehouseadlsms01.dfs.core.windows.net/raw_data/'
+# with (storage credentials `lakehouse-dbw-ms01`)
+
 # COMMAND ----------
 
